1e402c170c91d9b03e44dfc429cbdf6f7a109e95
christian baez <christian.baez@envirotime.cl>
2021-08-12 21:09:16 -0400

    java script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log ("hola alumnos")
+console.log ("hola alumnos");
+console.log (3+4);
+console.log (5-1);
+console.log (4*2);
+console.log (9/3);
+console.log ("amo" +" "+"codear.");
+function mostrarMensaje() {
+alert('apretaste el boton');
+}
+
+
